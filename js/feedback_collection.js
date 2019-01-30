2cc676761003638543827b22010e4819c76dd92e
martidor <martin2609@hotmail.com>
2019-01-30 17:02:17 +0100

    added firebase and some css

--- js/feedback_collection.js
+++ js/feedback_collection.js
@@ -1,13 +1,18 @@
-var feedback = [
-    {"comment": "Forelesningene er gode ooooooooooog konge og bra"},
-    {"comment": "Forelesningene er dårlige"},
-    {"comment": "Eksemplene er gode"},
-    {"comment": "Foilene er bra"}
-];
-
 $(document).ready(
     function() {
-        $("#js-feedbackInput").fuzzyComplete(feedback);
-        $(".prompt").fuzzyComplete(feedback);
+  
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyDZnTCsAF3coKF78vRtmaojn6oxY-a4yKI",
+            authDomain: "masteroppgave-ntnu.firebaseapp.com",
+            databaseURL: "https://masteroppgave-ntnu.firebaseio.com",
+            projectId: "masteroppgave-ntnu",
+            storageBucket: "masteroppgave-ntnu.appspot.com",
+            messagingSenderId: "803033729158"
+        };
+        
+        firebase.initializeApp(config);
+        const db = firebase.firestore();
+        
     }
 );
